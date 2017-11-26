af8c2200936dd3d8927bcc9342c02f684ed9a3d7
Andri Ginting <andris.ginting@gmail.com>
2017-11-26 13:06:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/andriginting/dbsqlite/Main2Activity.java
@@ -0,0 +1,93 @@
+package com.example.andriginting.dbsqlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView tvName, tvAge, tvPhoneNo, tvEmail, tvIsLoveMU;
+    Button btnSave;
+    private PrefClass mUserPreference;
+
+    private boolean isPreferenceEmpty = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tvName = findViewById(R.id.tv_name);
+        tvAge = findViewById(R.id.tv_age);
+        tvPhoneNo = (TextView)findViewById(R.id.tv_phone);
+        tvEmail = (TextView)findViewById(R.id.tv_email);
+        tvIsLoveMU = (TextView)findViewById(R.id.tv_is_love_mu);
+        btnSave = (Button)findViewById(R.id.btn_save);
+        btnSave.setOnClickListener(this);
+
+        mUserPreference = new PrefClass(this);
+
+        getSupportActionBar().setTitle("My User Preference");
+
+        showExistingPreference();
+
+    }
+
+    /*
+    Set tampilan menggunakan preferences
+     */
+    private void showExistingPreference(){
+        if (!TextUtils.isEmpty(mUserPreference.getName())){
+
+            tvName.setText(mUserPreference.getName());
+            tvAge.setText(String.valueOf(mUserPreference.getAge()));
+            tvIsLoveMU.setText(mUserPreference.isLoveMU() ? "Ya" : "Tidak");
+            tvEmail.setText(mUserPreference.getEmail());
+            tvPhoneNo.setText(mUserPreference.getPhoneNumber());
+
+            btnSave.setText("Ubah");
+        }else{
+            final String TEXT_EMPTY = "Tidak Ada";
+
+            tvName.setText(TEXT_EMPTY);
+            tvAge.setText(TEXT_EMPTY);
+            tvIsLoveMU.setText(TEXT_EMPTY);
+            tvEmail.setText(TEXT_EMPTY);
+            tvPhoneNo.setText(TEXT_EMPTY);
+
+            btnSave.setText("Simpan");
+
+            isPreferenceEmpty = true;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_save){
+            Intent intent = new Intent(Main2Activity.this,
+                    FormUserPreferenceActivity.class);
+            if (isPreferenceEmpty){
+                intent.putExtra(FormUserPreferenceActivity.EXTRA_TYPE_FORM,
+                        FormUserPreferenceActivity.TYPE_ADD);
+            }else{
+                intent.putExtra(FormUserPreferenceActivity.EXTRA_TYPE_FORM,
+                        FormUserPreferenceActivity.TYPE_EDIT);
+            }
+            startActivityForResult(intent, FormUserPreferenceActivity.REQUEST_CODE);
+        }
+    }
+
+    /*
+    Akan dipanggil ketika formuserpreferenceactivity ditutup
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FormUserPreferenceActivity.REQUEST_CODE){
+            showExistingPreference();
+        }
+    }
+}
